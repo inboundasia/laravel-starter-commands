c6af3666c6775e37a2252471c03fdace398a8bf1
kuro roli <kuroridoplayer@gmail.com>
2023-03-19 15:59:53 +0800

    change version to 1.0.1

---
+++ version.php
@@ -0,0 +1,35 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+function ask(string $question, string $default = ''): string
+{
+    $answer = readline($question.($default ? " ({$default})" : null).': ');
+
+    if (! $answer) {
+        return $default;
+    }
+
+    return $answer;
+}
+
+$composer = json_decode(file_get_contents('composer.json'), true);
+$version = new \PHLAK\SemVer\Version($composer['version']);
+
+$type = ask('major/minor/patch?', 'patch');
+
+if ($type === 'patch') {
+    $version->incrementPatch();
+} else if ($type === 'minor') {
+    $version->incrementMinor();
+} else if ($type === 'major') {
+    $version->incrementMajor();
+}
+
+$composer['version'] = (string) $version;
+file_put_contents('composer.json', json_encode($composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+
+shell_exec("git add .");
+shell_exec("git commit -m 'change version to $version'");
+shell_exec("git tag $version");
+shell_exec("git push --tags");
